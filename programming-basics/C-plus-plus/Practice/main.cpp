15a68fb88eb795a0ddc3daf8b0cef567583bb972
doichev-kostia <kostynd2004@gmail.com>
2021-10-05 13:32:38 +0300

    Edited function naming

--- programming-basics/C-plus-plus/Practice/main.cpp
+++ programming-basics/C-plus-plus/Practice/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 using namespace std;
-int res (int A, int B, int C, int D);
+int calculateByFormula (int A, int B, int C, int D);
 
 int main() {
     int numbers[] = {6, 7, 7, 14};
-    int result = res(numbers[0], numbers[1], numbers[2], numbers[3]);
+    int result = calculateByFormula(numbers[0], numbers[1], numbers[2], numbers[3]);
     cout << "The result is: " << result << endl;
     return 0;
 }
 
-int res (int A, int B, int C, int D){
+int calculateByFormula (int A, int B, int C, int D){
     int result = (!(A == B) && (!(C != D)));
     return result;
 }
