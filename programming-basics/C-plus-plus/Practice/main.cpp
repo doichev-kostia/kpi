59eb62c8c94c7ecd78bc08b8718f18a4e411cc0e
doichev-kostia <kostynd2004@gmail.com>
2021-10-05 13:35:19 +0300

    Edited function data type

--- programming-basics/C-plus-plus/Practice/main.cpp
+++ programming-basics/C-plus-plus/Practice/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 
 using namespace std;
-int calculateByFormula (int A, int B, int C, int D);
+bool calculateByFormula (int A, int B, int C, int D);
 
 int main() {
     int numbers[] = {6, 7, 7, 14};
-    int result = calculateByFormula(numbers[0], numbers[1], numbers[2], numbers[3]);
+    bool result = calculateByFormula(numbers[0], numbers[1], numbers[2], numbers[3]);
     cout << "The result is: " << result << endl;
     return 0;
 }
 
-int calculateByFormula (int A, int B, int C, int D){
-    int result = (!(A == B) && (!(C != D)));
-    return result;
+bool calculateByFormula (int A, int B, int C, int D){
+   return (!(A == B) && (!(C != D)));
 }
