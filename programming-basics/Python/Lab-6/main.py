8e5beaf1e5e7a4b07a560087da513958a2727f2d
doichev-kostia <kostynd2004@gmail.com>
2021-11-26 14:39:38 +0200

    completed 6th python lab

---
+++ programming-basics/Python/Lab-6/main.py
@@ -0,0 +1,41 @@
+import math
+
+def calculate_step(a, b, n):
+    return (b - a) / n
+
+def calculate_sin (a, b, n):
+    h = calculate_step(a, b, n)
+    sum = 0
+    for i in range(1, n):
+        sum += pow(math.atan(a + i * h), 2)
+
+    func_sum = pow(math.atan(a), 2) + pow(math.atan(b), 2)
+    return h * (func_sum / 2 + sum)
+
+
+def calculate_arctg (a, b, n):
+    h = calculate_step(a, b, n)
+    sum = 0
+    for i in range(1, n):
+        exponent = 10 * (a + i * h)
+        sum += math.sin(math.pow(math.e, exponent))
+
+    func_sum = math.sin(math.pow(math.e, 10 * a)) + math.sin(math.pow(math.e, 10 * b))
+    return h * (func_sum / 2 + sum)
+
+def calculate_integral(a,b,n):
+    return calculate_arctg(a, b, n) + calculate_sin(0, math.pi, n)
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+n = int(input("Enter natural n: "))
+
+while n <= 0:
+    n = int(input("Enter natural n: "))
+
+res = calculate_integral(a, b, n)
+
+print(res)
+
+
