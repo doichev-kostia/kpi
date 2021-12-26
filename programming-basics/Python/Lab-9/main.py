6951f0723a100398f4149b7b33f724bb002f62f8
doichev-kostia <kostynd2004@gmail.com>
2021-12-26 22:15:45 +0200

    pb 9lab: completed python version

---
+++ programming-basics/Python/Lab-9/main.py
@@ -0,0 +1,14 @@
+import re
+input_string = input("Enter the sentence: ")
+character_group = input("Enter characters: ")
+
+words = re.split(r'[(!,.:?\')]|[( +)]', input_string)
+filtered_words = []
+
+for word in words:
+    if not word.endswith(character_group) or word == " ":
+        filtered_words.append(word)
+number_of_deletions = len(words) - len(filtered_words)
+
+formatted_string = " ".join(filtered_words)
+print("Result: ", formatted_string)
