e515147dd61b645dd06b67ed83d7344b60245c81
doichev-kostia <kostynd2004@gmail.com>
2021-12-26 23:36:44 +0200

    pb lab9: completed C++ version

--- programming-basics/C-plus-plus/Lab-9/main.cpp
+++ programming-basics/C-plus-plus/Lab-9/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include <iomanip>
-#include <cstdio>
 
 typedef struct {
     std::string value;
     size_t length;
 } str;
-
-template<typename T>
-struct array {
-    T *value;
-    size_t length;
-};
 
 
 bool isPunctuationMark(int character);
@@ -30,78 +22,27 @@ int main() {
 
     inputString.length = inputString.value.length();
     characterGroup.length = characterGroup.value.length();
+    std::size_t foundIndex;
+    std::string inputStringCopy = inputString.value;
+    int numberOfSubtractions = 0;
+    do {
+        foundIndex = inputStringCopy.find(characterGroup.value);
+        if (foundIndex != std::string::npos) {
+            for (size_t i = foundIndex; i >= 0; --i) {
+                int currentCharacter = tolower(inputStringCopy[i]);
+                bool isPunctuation = isPunctuationMark(currentCharacter);
+                bool isLast = i == 0;
 
-    int numberOfWords = 0;
-    for (int i = 0; i < inputString.length; ++i) {
-        int currentCharacter = tolower(inputString.value[i]);
-        int prevCharacter = i > 0 ? tolower(inputString.value[i - 1]) : 0;
-        bool isPunctuation = isPunctuationMark(currentCharacter);
-        bool isPreviousCharacterPunctuation = isPunctuationMark(prevCharacter);
-
-        if (i == 0 && isPunctuation) {
-            continue;
-        }
-
-        if ((isPunctuation && !isPreviousCharacterPunctuation) || i == inputString.length - 1) {
-            numberOfWords++;
-        }
-    }
-
-    array<array<char>> words;
-    words.value = new array<char>[numberOfWords];
-    words.length = numberOfWords;
-    int wordOrder = 0;
-    char *word;
-    int wordLength = 0;
-    for (int i = 0; i <= inputString.length; ++i) {
-        int currentCharacter = tolower(inputString.value[i]);
-
-        char *temporary;
-        if (wordLength != 0) {
-            temporary = new char[wordLength];
-            for (int j = 0; j < wordLength; ++j) {
-                temporary[j] = word[j];
+                if (isPunctuation || isLast) {
+                    inputStringCopy.replace(i, foundIndex + characterGroup.length, "");
+                    numberOfSubtractions++;
+                    break;
+                }
             }
         }
-
-        int prevCharacter = i > 0 ? tolower(inputString.value[i - 1]) : 0;
-        bool isPunctuation = isPunctuationMark(currentCharacter);
-        bool isPreviousCharacterPunctuation = isPunctuationMark(prevCharacter);
-
-        if (wordLength != 0 && !isPunctuation) {
-            delete[] word;
-        }
-
-        word = new char[wordLength + 1];
-        if (wordLength > 0) {
-            for (int j = 0; j < wordLength; ++j) {
-                word[j] = temporary[j];
-            }
-            delete[] temporary;
-        }
-
-        if ((isPunctuation && !isPreviousCharacterPunctuation) || i == inputString.length) {
-            array<char> wordItem;
-            word[wordLength] = '\0';
-            wordItem.length = wordLength + 1;
-            wordItem.value = word;
-            words.value[wordOrder] = wordItem;
-            wordLength = 0;
-            wordOrder++;
-            delete[] word;
-        } else if (!isPunctuation) {
-            word[wordLength] = (char) currentCharacter;
-            ++wordLength;
-        }
-    }
-
-    for (int i = 0; i < words.length; ++i) {
-        array<char> word = words.value[i];
-        for (int j = 0; j < word.length; ++j) {
-            std::cout << word.value[j] << std::endl;
-        }
-        std::cout << std::endl;
-    }
+    } while (foundIndex != std::string::npos);
+    std::cout << "Result: " << inputStringCopy << std::endl;
+    std::cout << "Number of subtractions: " << numberOfSubtractions << std::endl;
 
     return 0;
 }
