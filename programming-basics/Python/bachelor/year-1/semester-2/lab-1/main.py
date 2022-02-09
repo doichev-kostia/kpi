5123cc911f452d2e3a8ff91824408f62711976f6
doichev-kostia <kostynd2004@gmail.com>
2022-02-09 23:42:38 +0200

    Completed 1 python lab

---
+++ programming-basics/Python/bachelor/year-1/semester-2/lab-1/main.py
@@ -0,0 +1,30 @@
+import utils
+
+
+def main():
+    input_text = utils.get_text()
+
+    filename = input("Enter the file name: ")
+    input_mode = input("Would you like to append the text or replace the old one? Enter 'a' for appending or 'w' for replacing: ")
+    utils.fill_file(filename, input_mode.strip(), input_text)
+    print("The file has been modified! Open {}".format(filename))
+
+    print('----------------------------------------------------')
+    print('Input text: {}'.format(input_text))
+    print('----------------------------------------------------')
+
+    searching_char = input("Enter the searching character: ")
+    case_sensitive_result = input("Should the filter be case sensitive? Enter 'y' for yes or 'n' for no: ")
+    is_case_sensitive = True if case_sensitive_result.strip() == 'y' else False
+    new_filename = input("Enter the file name where the result will be stored: ")
+    output_mode = input("Would you like to append the text or replace the old one? Enter 'a' for appending or 'w' for replacing: ")
+    filtered_words = utils.filter_words(input_text, searching_char.strip(), is_case_sensitive)
+    utils.fill_file(new_filename, output_mode, filtered_words)
+    print("The file has been modified! Open {}".format(new_filename))
+
+    print('----------------------------------------------------')
+    print('Output text: {}'.format(filtered_words))
+    print('----------------------------------------------------')
+
+
+main()
---
+++ programming-basics/Python/bachelor/year-1/semester-2/lab-1/utils.py
@@ -0,0 +1,31 @@
+def get_text():
+    lines = []
+    terminating_key = input("Enter the key that will finish the input: ")
+    print("Enter the text. To finish entering press '{}' and Enter".format(terminating_key))
+    while True:
+        line = input()
+        if line.find(terminating_key) != -1:
+            lines.append(line[0:-2])
+            break
+        else:
+            lines.append(line)
+    return "\n".join(lines)
+
+def fill_file(filename, mode, content):
+    file = open(filename, mode)
+    file.write(content)
+    file.close()
+    return file
+
+def filter_words(text, filtering_char, is_case_sensitive):
+    filtered = []
+    words = text.split()
+    for word in words:
+        current_word = word
+        if not is_case_sensitive:
+            current_word = word.lower()
+            filtering_char = filtering_char.lower()
+
+        if current_word.startswith(filtering_char):
+            filtered.append(word)
+    return " ".join(filtered) + " "
