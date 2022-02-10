1368ffea26d899a007ff08c0570bc09e9ad65471
doichev-kostia <kostynd2004@gmail.com>
2022-02-10 13:55:44 +0200

    fixed get_text function in python lab 1

--- programming-basics/Python/bachelor/year-1/semester-2/lab-1/utils.py
+++ programming-basics/Python/bachelor/year-1/semester-2/lab-1/utils.py
@@ -2,20 +2,23 @@ def get_text():
     lines = []
     terminating_key = input("Enter the key that will finish the input: ")
     print("Enter the text. To finish entering press '{}' and Enter".format(terminating_key))
+    terminating_key_length = len(terminating_key)
     while True:
         line = input()
         if line.find(terminating_key) != -1:
-            lines.append(line[0:-2])
+            lines.append(line[0:-1 * terminating_key_length])
             break
         else:
             lines.append(line)
     return "\n".join(lines)
+
 
 def fill_file(filename, mode, content):
     file = open(filename, mode)
     file.write(content)
     file.close()
     return file
+
 
 def filter_words(text, filtering_char, is_case_sensitive):
     filtered = []
