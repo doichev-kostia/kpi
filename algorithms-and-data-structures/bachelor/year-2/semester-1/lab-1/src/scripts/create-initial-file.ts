1a10ae8c2f789c8711792121ffa0d0477b620e73
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2022-10-02 19:28:41 +0200

    feat: ads lab-1 init

---
+++ algorithms-and-data-structures/bachelor/year-2/semester-1/lab-1/src/scripts/create-initial-file.ts
@@ -0,0 +1,63 @@
+import fs from "fs";
+import yargs from "yargs";
+import {hideBin} from "yargs/helpers";
+
+const MIN_FILE_SIZE_IN_BYTES = 10 * 1024 * 1024;
+const INT_SIZE_IN_BYTES = 4;
+const MAX_INT = Math.pow(2, 32) / 2 - 1;
+const MIN_INT = -Math.pow(2, 32) / 2;
+
+const argv = yargs(hideBin(process.argv))
+    .usage("Usage: $0 --file [string] --size [number] --min [number] --max [number]")
+    .options({
+        file: {type: "string", demandOption: true, alias: "f", description: "File name"},
+        size: {
+            type: "number",
+            alias: "s",
+            default: MIN_FILE_SIZE_IN_BYTES,
+            description: "File size in bytes",
+            defaultDescription: "10MB"
+        },
+        min: {type: "number", default: 0, description: "Min integer"},
+        max: {type: "number", default: 1000, description: "Max integer"},
+    })
+    .check(({ size, min, max}) =>{
+        if (size < MIN_FILE_SIZE_IN_BYTES) {
+            throw new Error(`File size should be at least ${MIN_FILE_SIZE_IN_BYTES} bytes`);
+        }
+        if (min > max) {
+            throw new Error("Min should be less than max");
+        }
+
+        if (!Number.isInteger(min) || !Number.isInteger(max)) {
+            throw new Error("Min and max should be integers");
+        }
+
+        if (max > MAX_INT) {
+            throw new Error(`Max should be less or equal to the positive signed 32-bit integer. Max value is ${MAX_INT}`);
+        }
+
+        if (min < MIN_INT) {
+            throw new Error(`Min should be greater of equal to the negative signed 32-bit integer. Min value is ${MIN_INT}`);
+        }
+
+        return true;
+    })
+    .help('h')
+    .alias('h', 'help')
+    .parseSync();
+
+const randomInteger = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const createFile = () => {
+    const {file, max, min, size} = argv;
+    const writeStream = fs.createWriteStream(file, {flags: "w", encoding: "binary"});
+    const integers = new Array(Math.floor(size / INT_SIZE_IN_BYTES)).fill(0).map(() => randomInteger(min, max));
+    const buffer = Buffer.from(Int32Array.from(integers).buffer);
+    writeStream.write(buffer, "binary");
+    writeStream.end();
+}
+
+createFile();
