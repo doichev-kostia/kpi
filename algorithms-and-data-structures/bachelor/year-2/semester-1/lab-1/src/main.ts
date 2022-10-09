12cd97c5d368e36287ce079ac1e0e0a6f72876f7
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2022-10-09 20:03:02 +0200

    feat: external sort

--- algorithms-and-data-structures/bachelor/year-2/semester-1/lab-1/src/main.ts
+++ algorithms-and-data-structures/bachelor/year-2/semester-1/lab-1/src/main.ts
@@ -1,12 +1,8 @@
 import path from "path";
 import fs from "fs";
 
-// const MAX_BUFFER_SIZE_IN_BYTES = 20 * 1024 * 1024;
 const file = path.join(__dirname, "../files/data.bin");
-// const output = path.join(__dirname, "../files/output.bin");
-
-// init:  [33, 48, 24, 20, 20, 8, 33, 22, 46, 16, 29, 8, 24, 17, 5, 39, 31, 47, 44, 4]
-// final: [4, 5, 8, 8, 16, 17, 20, 20, 22, 24, 24, 29, 31, 33, 33, 39, 44, 46, 47, 48]
+const output = path.join(__dirname, "../files/output.bin");
 
 const getClosestFibonacciSequence = (number: number) => {
     let first = 0;
@@ -39,7 +35,7 @@ const getChunks = (integers: number[]) => {
             }
         }
 
-        if (isLast && integers[i] < integers[i + 1]) {
+        if (isLast && integers[i] <= integers[i + 1]) {
             tmp.push(integers[i + 1]);
             chunks.push(tmp);
         }
@@ -47,19 +43,35 @@ const getChunks = (integers: number[]) => {
     return chunks;
 }
 
+const displayChunkOfFile = async (filePath: string, chunkSize: number) => {
+    const readStream = fs.createReadStream(filePath, {highWaterMark: chunkSize});
+    let iteration = 0;
+    readStream.on('data', (chunk) => {
+        const data = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+        const integers = Array.from(new Int32Array(data.buffer));
+        if (iteration === 1) {
+            readStream.destroy();
+            return;
+        }
+        console.log(integers);
+        iteration++;
+    })
+
+}
+
 const sortExternally = async (initialFilePath: string, tmpDirPath: string) => {
+    console.time("Sort time");
     const data = await fs.promises.readFile(initialFilePath);
     const integers = Array.from(new Int32Array(data.buffer));
     const firstFilePath = path.join(tmpDirPath, 'first.bin');
     const secondFilePath = path.join(tmpDirPath, 'second.bin');
     const finalFilePath = path.join(tmpDirPath, 'final.bin');
-    const chunks = getChunks(integers);
 
-    console.log(`Number of chunks: ${chunks.length}`);
+    const chunks = getChunks(integers);
 
     const {first, second, final} = getClosestFibonacciSequence(chunks.length);
     const firstFileContent = chunks.slice(0, first).flat();
-    await fs.promises.writeFile( firstFilePath, Buffer.from(Int32Array.from(firstFileContent).buffer), {
+    await fs.promises.writeFile(firstFilePath, Buffer.from(Int32Array.from(firstFileContent).buffer), {
         encoding: 'binary',
         flag: 'w'
     });
@@ -69,46 +81,81 @@ const sortExternally = async (initialFilePath: string, tmpDirPath: string) => {
         flag: 'w'
     });
 
-    const firstFileBuffer = await fs.promises.readFile(firstFilePath);
-    const firstFileIntegers = Array.from(new Int32Array(firstFileBuffer.buffer));
-    const secondFileBuffer = await fs.promises.readFile(secondFilePath);
-    const secondFileIntegers = Array.from(new Int32Array(secondFileBuffer.buffer));
+    let sortedFilePath = ''
+
+    const polyphaseMerge = async (firstFilePath: string, secondFilePath: string, finalFilePath: string) => {
+        const firstFileBuffer = await fs.promises.readFile(firstFilePath);
+        const secondFileBuffer = await fs.promises.readFile(secondFilePath);
 
 
-    const firstChunks: number[][] = getChunks(firstFileIntegers);
-    const secondChunks: number[][] = getChunks(secondFileIntegers);
+        if (firstFileBuffer.length === 0 && secondFileBuffer.length === 0) {
+            return;
+        }
 
-    const delta = firstChunks.length - secondChunks.length;
+        const firstFileIntegers = Array.from(new Int32Array(firstFileBuffer.buffer));
+        const secondFileIntegers = Array.from(new Int32Array(secondFileBuffer.buffer));
 
-    const finalChunks: number[][] = [];
-    let iterableChunks: number[][] = firstChunks;
-    if (delta > 0) {
-        iterableChunks = secondChunks;
+
+        const firstChunks: number[][] = getChunks(firstFileIntegers);
+        const secondChunks: number[][] = getChunks(secondFileIntegers);
+
+        const delta = firstChunks.length - secondChunks.length;
+
+        const finalChunks: number[][] = [];
+        let iterableChunks: number[][] = firstChunks;
+        if (delta > 0) {
+            iterableChunks = secondChunks;
+        }
+
+        for (let i = 0; i < iterableChunks.length; i++) {
+            const firstChunk = firstChunks[i];
+            const secondChunk = secondChunks[i];
+            finalChunks.push([...firstChunk, ...secondChunk].sort((a, b) => a - b));
+        }
+
+        let emptyFilePath = '';
+        if (delta > 0) {
+            const buffer = Buffer.from(new Int32Array(firstChunks.slice(iterableChunks.length).flat()).buffer);
+            await fs.promises.writeFile(firstFilePath, buffer, {encoding: 'binary', flag: 'w'});
+
+            // tmp
+            emptyFilePath = secondFilePath;
+            await fs.promises.writeFile(secondFilePath, Buffer.from(new Int32Array(0).buffer), {
+                encoding: 'binary',
+                flag: 'w'
+            });
+        } else if (delta < 0) {
+            const buffer = Buffer.from(new Int32Array(secondChunks.slice(iterableChunks.length).flat()).buffer);
+            await fs.promises.writeFile(secondFilePath, buffer, {encoding: 'binary', flag: 'w'});
+
+            // tmp
+            emptyFilePath = firstFilePath;
+            await fs.promises.writeFile(firstFilePath, Buffer.from(new Int32Array(0).buffer), {
+                encoding: 'binary',
+                flag: 'w'
+            });
+        }
+
+
+        const finalFileContent = finalChunks.flat();
+        await fs.promises.writeFile(finalFilePath, Buffer.from(Int32Array.from(finalFileContent).buffer));
+
+        if (delta === 0) {
+            sortedFilePath = finalFilePath;
+            return;
+        }
+        const [first, second] = [firstFilePath, secondFilePath, finalFilePath].filter((path) => path !== emptyFilePath);
+
+        await polyphaseMerge(first, second, emptyFilePath);
     }
 
-    for (let i = 0; i < iterableChunks.length; i++) {
-        const firstChunk = firstChunks[i];
-        const secondChunk = secondChunks[i];
-        finalChunks.push([...firstChunk, ...secondChunk].sort((a, b) => a - b));
-    }
+    await polyphaseMerge(firstFilePath, secondFilePath, finalFilePath);
+    console.timeEnd("Sort time");
 
-    if (delta > 0) {
-        const buffer = Buffer.from(new Int32Array(firstChunks.slice(iterableChunks.length).flat()).buffer);
-        await fs.promises.writeFile(firstFilePath, buffer, { encoding: 'binary', flag: 'w' });
-        // tmp
-        await fs.promises.writeFile(secondFilePath, Buffer.from(new Int32Array(0).buffer), { encoding: 'binary', flag: 'w' });
-    } else if (delta < 0) {
-       const buffer = Buffer.from(new Int32Array(secondChunks.slice(iterableChunks.length).flat()).buffer);
-       await fs.promises.writeFile(secondFilePath, buffer, { encoding: 'binary', flag: 'w' });
-
-        // tmp
-        await fs.promises.writeFile(firstFilePath, Buffer.from(new Int32Array(0).buffer), { encoding: 'binary', flag: 'w' });
-    }
-
-    const finalFileContent = finalChunks.flat();
-    console.log({integers, finalFileContent});
-    await fs.promises.writeFile(finalFilePath, Buffer.from(Int32Array.from(finalFileContent).buffer));
+    await fs.promises.copyFile(sortedFilePath, output);
+    await fs.promises.rm(tmpDirPath, {recursive: true, force: true});
 }
+
 
 export const main = async (filePath: string) => {
     const fileStats = await fs.promises.stat(filePath);
@@ -118,7 +165,7 @@ export const main = async (filePath: string) => {
             await fs.promises.access(path.join(__dirname, '../files/tmp'));
             return path.join(__dirname, '../files/tmp');
         } catch (_) {
-            await fs.promises.mkdir(path.join(__dirname, "../files/tmp"), {recursive: true});
+            return await fs.promises.mkdir(path.join(__dirname, "../files/tmp"), {recursive: true});
         }
     })();
 
@@ -127,7 +174,15 @@ export const main = async (filePath: string) => {
         process.exit(1);
     }
 
+    console.log("60 bytes of the initial file");
+    await displayChunkOfFile(filePath, 60);
+
+    console.log("Sorting file...");
     await sortExternally(filePath, tmp);
+    console.log("Done");
+
+    console.log("60 bytes of the final file");
+    await displayChunkOfFile(output, 60);
 }
 
 main(file);
