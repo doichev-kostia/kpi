23a0e773272d407e820f0fbc8e32f22b81557bae
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2022-10-09 11:39:13 +0200

    feat: node streams for file generation

--- algorithms-and-data-structures/bachelor/year-2/semester-1/lab-1/src/scripts/create-initial-file.ts
+++ algorithms-and-data-structures/bachelor/year-2/semester-1/lab-1/src/scripts/create-initial-file.ts
@@ -3,6 +3,7 @@ import yargs from "yargs";
 import {hideBin} from "yargs/helpers";
 
 const MIN_FILE_SIZE_IN_BYTES = 10 * 1024 * 1024;
+const MAX_BUFFER_SIZE_IN_BYTES = 20 * 1024 * 1024;
 const INT_SIZE_IN_BYTES = 4;
 const MAX_INT = Math.pow(2, 32) / 2 - 1;
 const MIN_INT = -Math.pow(2, 32) / 2;
@@ -21,7 +22,7 @@ const argv = yargs(hideBin(process.argv))
         min: {type: "number", default: 0, description: "Min integer"},
         max: {type: "number", default: 1000, description: "Max integer"},
     })
-    .check(({ size, min, max}) =>{
+    .check(({size, min, max}) => {
         if (size < MIN_FILE_SIZE_IN_BYTES) {
             throw new Error(`File size should be at least ${MIN_FILE_SIZE_IN_BYTES} bytes`);
         }
@@ -54,9 +55,24 @@ const randomInteger = (min: number, max: number) => {
 const createFile = () => {
     const {file, max, min, size} = argv;
     const writeStream = fs.createWriteStream(file, {flags: "w", encoding: "binary"});
-    const integers = new Array(Math.floor(size / INT_SIZE_IN_BYTES)).fill(0).map(() => randomInteger(min, max));
-    const buffer = Buffer.from(Int32Array.from(integers).buffer);
-    writeStream.write(buffer, "binary");
+    if (size < MAX_BUFFER_SIZE_IN_BYTES) {
+        const integers = new Array(Math.floor(size / INT_SIZE_IN_BYTES)).fill(0).map(() => randomInteger(min, max));
+        const buffer = Buffer.from(Int32Array.from(integers).buffer);
+        writeStream.write(buffer, "binary");
+        writeStream.end();
+        return;
+    }
+
+    let remainingBytes = size;
+    const parts = Math.floor(size / MAX_BUFFER_SIZE_IN_BYTES);
+    for (let i = 0; i < parts; i++) {
+        const size = remainingBytes > MAX_BUFFER_SIZE_IN_BYTES ? MAX_BUFFER_SIZE_IN_BYTES : remainingBytes;
+        const integers = new Array(Math.floor(size / INT_SIZE_IN_BYTES)).fill(0).map(() => randomInteger(min, max));
+        const buffer = Buffer.from(Int32Array.from(integers).buffer);
+        writeStream.write(buffer, "binary");
+        remainingBytes -= size;
+    }
+
     writeStream.end();
 }
 
