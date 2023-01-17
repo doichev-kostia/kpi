4b64f688da5a391f625e6664d14d7b06ae41d270
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-01-17 22:43:03 +0100

    feat: card

---
+++ algorithms-and-data-structures/bachelor/year-2/semester-1/lab-6/src/card.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import cx from 'classnames';
+import './card.css';
+import {CardSuit, CardValue} from "./deck";
+
+
+interface CardProps {
+	value: CardValue;
+	suit: CardSuit;
+	faceUp?: boolean;
+}
+
+export const Card = ({value, suit, faceUp = false}: CardProps) => {
+
+	const valueAsNumber = parseInt(value);
+	const isFaceCardValue = Number.isNaN(valueAsNumber);
+
+	return (
+		<div className={cx('card', !faceUp && 'bg-gray-500')} data-value={value} data-suit={suit}>
+			{faceUp && (
+				<>
+					{
+						isFaceCardValue ? (
+							<div className="card__pip"></div>
+						) : (
+							new Array(parseInt(value)).fill(0).map((_, i) => (
+								<div key={i} className="card__pip"></div>
+							))
+						)
+					}
+					<div className={cx('card__corner-number', "top-1 left-1")}>{value}</div>
+					<div className={cx('card__corner-number', "bottom-1 right-1 rotate-180")}>{value}</div>
+				</>
+			)}
+		</div>
+	);
+};
