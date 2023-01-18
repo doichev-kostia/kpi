1f8f5a41f62422ac90fdba88897d8ddd0f1c524e
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-01-18 12:43:26 +0100

    feat: updated constants

--- algorithms-and-data-structures/bachelor/year-2/semester-1/lab-5/src/constants.ts
+++ algorithms-and-data-structures/bachelor/year-2/semester-1/lab-5/src/constants.ts
@@ -1,12 +1,12 @@
-export const NUMBER_OF_CITIES = 200;
+export const NUMBER_OF_CITIES = 300;
 export const NUMBER_OF_ANTS = 45;
 export const MIN_DISTANCE = 5;
-export const MAX_DISTANCE = 150
+export const MAX_DISTANCE = 150;
 export const INITIAL_PHEROMONE = 0.1;
 export const CONSTANT_ARGUMENTS = {
-    alpha: 3,
-    beta: 2,
-    p: 0.3,
+  alpha: 3,
+  beta: 2,
+  p: 0.3,
 };
 
 export const RESULTS_CHECKPOINT = 20;
@@ -18,4 +18,3 @@ export const NUMBER_OF_ITERATIONS = 1000;
  * pheromone[i][j] = (1 - p) * pheromone[i][j] + deltaPheromone[i][j]
  */
 export const PHEROMONE_DISAPPEARANCE_COEFFICIENT = 1 - CONSTANT_ARGUMENTS.p;
-
