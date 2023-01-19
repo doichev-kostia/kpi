d7fb2dc83445ab3d61c225b5c28bc0f55982e98a
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-01-19 18:52:52 +0100

    feat: toast container

--- algorithms-and-data-structures/bachelor/year-2/semester-1/lab-6/src/main.tsx
+++ algorithms-and-data-structures/bachelor/year-2/semester-1/lab-6/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './normalize.css'
 import './index.css'
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App/>
+		<ToastContainer/>
+	</React.StrictMode>,
 )
